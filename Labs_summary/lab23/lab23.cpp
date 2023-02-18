7cf9305718daf6afa2fecbfb7f627c4261a2a899
Maxsmile123TEST <Sysoev_maksimka@bk.ru>
2023-02-18 10:58:47 +0300

    lol

--- Labs_summary/lab23/lab23.cpp
+++ Labs_summary/lab23/lab23.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main(){
-    std::cout << "This is work lab, i'm promise you" << std::endl;
+int main()
+{
+    std::cout << "This is work lab, i'm promise you\n" << std::endl;
     return 0;
 }
